e9592858c1f3cde8f9926ecfbfd1a6fd7e6a4fed
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-03-31 17:33:11 -0300

    chore(lint): fix unnecessary template literals and unused parameter warnings

--- src/http/controllers/dealershipController.ts
+++ src/http/controllers/dealershipController.ts
@@ -1,14 +1,16 @@
 import { handler } from '../../_lib/http/handler.js';
 import { DealershipModel } from '../../database/models/DealershipModel.js';
 
-const index = handler(async (request, reply) => {
+const index = handler(async (_request, reply) => {
   const dealerships = await DealershipModel.query();
 
   return reply.view('dealerships/index', { dealerships });
 });
 
-const create = handler(async (request, reply) => {
-  return reply.view('dealerships/create', { dealership: new DealershipModel() });
+const create = handler(async (_request, reply) => {
+  return reply.view('dealerships/create', {
+    dealership: new DealershipModel(),
+  });
 });
 
 const store = handler<{ Body: { name: string } }>(async (request, reply) => {
@@ -17,15 +19,19 @@ const store = handler<{ Body: { name: string } }>(async (request, reply) => {
   try {
     await DealershipModel.query().insert({ name });
 
-    return reply.redirect(`/dealerships`);
+    return reply.redirect('dealerships');
   } catch (error) {
     console.error(error);
-    return reply.view('dealerships/create', { dealership: new DealershipModel().$set({ name }) });
+    return reply.view('dealerships/create', {
+      dealership: new DealershipModel().$set({ name }),
+    });
   }
 });
 
 const edit = handler<{ Params: { id: string } }>(async (request, reply) => {
-  const dealership = await DealershipModel.query().findById(request.params.id).throwIfNotFound();
+  const dealership = await DealershipModel.query()
+    .findById(request.params.id)
+    .throwIfNotFound();
 
   return reply.view('dealerships/update', { dealership });
 });
@@ -34,14 +40,16 @@ const update = handler<{
   Params: { id: string };
   Body: { name: string };
 }>(async (request, reply) => {
-  const dealership = await DealershipModel.query().findById(request.params.id).throwIfNotFound();
+  const dealership = await DealershipModel.query()
+    .findById(request.params.id)
+    .throwIfNotFound();
   const { name } = request.body;
   const newDealership = dealership.$set({ name });
 
   try {
     await newDealership.$query().update();
 
-    return reply.redirect(`/dealerships`);
+    return reply.redirect('/dealerships');
   } catch (error) {
     console.error(error);
     return reply.view('dealerships/update', { dealership: newDealership });
@@ -50,9 +58,12 @@ const update = handler<{
 
 const destroy = handler<{ Params: { id: string } }>(async (request, reply) => {
   try {
-    await DealershipModel.query().findById(request.params.id).throwIfNotFound().delete();
+    await DealershipModel.query()
+      .findById(request.params.id)
+      .throwIfNotFound()
+      .delete();
 
-    return reply.redirect(`/dealerships`);
+    return reply.redirect('/dealerships');
   } catch (error) {
     console.error(error);
     return reply.redirect('/dealerships');
--- src/http/controllers/vehiclesController.ts
+++ src/http/controllers/vehiclesController.ts
@@ -1,49 +1,90 @@
 import { handler } from '../../_lib/http/handler.js';
 import { VehicleModel } from '../../database/models/VehicleModel.js';
 
-const index = handler(async (request, reply) => {
+const index = handler(async (_request, reply) => {
   const vehicles = await VehicleModel.query();
 
   return reply.view('vehicles/index', { vehicles });
 });
 
-const create = handler(async (request, reply) => {
+const create = handler(async (_request, reply) => {
   return reply.view('vehicles/create', { vehicle: new VehicleModel() });
 });
 
 const store = handler<{
-  Body: { name: string, brand: string, model: string, year: string, comments: string, dealershipId: number };
+  Body: {
+    name: string;
+    brand: string;
+    model: string;
+    year: string;
+    comments: string;
+    dealershipId: number;
+  };
 }>(async (request, reply) => {
   const { name, brand, model, year, comments, dealershipId } = request.body;
 
   try {
-    await VehicleModel.query().insert({ name, brand, model, year, comments, dealershipId });
+    await VehicleModel.query().insert({
+      name,
+      brand,
+      model,
+      year,
+      comments,
+      dealershipId,
+    });
 
-    return reply.redirect(`/vehicles`);
+    return reply.redirect('/vehicles');
   } catch (error) {
     console.error(error);
-    return reply.view('vehicles/create', { vehicle: new VehicleModel().$set({ name, brand, model, year, comments, dealershipId }) });
+    return reply.view('vehicles/create', {
+      vehicle: new VehicleModel().$set({
+        name,
+        brand,
+        model,
+        year,
+        comments,
+        dealershipId,
+      }),
+    });
   }
 });
 
 const edit = handler<{ Params: { id: string } }>(async (request, reply) => {
-  const vehicle = await VehicleModel.query().findById(request.params.id).throwIfNotFound();
+  const vehicle = await VehicleModel.query()
+    .findById(request.params.id)
+    .throwIfNotFound();
 
   return reply.view('vehicles/update', { vehicle });
 });
 
 const update = handler<{
   Params: { id: string };
-  Body: { name: string, brand: string, model: string, year: string, comments: string, dealershipId: number };
+  Body: {
+    name: string;
+    brand: string;
+    model: string;
+    year: string;
+    comments: string;
+    dealershipId: number;
+  };
 }>(async (request, reply) => {
-  const vehicle = await VehicleModel.query().findById(request.params.id).throwIfNotFound();
+  const vehicle = await VehicleModel.query()
+    .findById(request.params.id)
+    .throwIfNotFound();
   const { name, brand, model, year, comments, dealershipId } = request.body;
-  const newVehicle = vehicle.$set({ name, brand, model, year, comments, dealershipId });
+  const newVehicle = vehicle.$set({
+    name,
+    brand,
+    model,
+    year,
+    comments,
+    dealershipId,
+  });
 
   try {
     await newVehicle.$query().update();
 
-    return reply.redirect(`/vehicles`);
+    return reply.redirect('/vehicles');
   } catch (error) {
     console.error(error);
     return reply.view('vehicles/update', { vehicle: newVehicle });
@@ -52,9 +93,12 @@ const update = handler<{
 
 const destroy = handler<{ Params: { id: string } }>(async (request, reply) => {
   try {
-    await VehicleModel.query().findById(request.params.id).throwIfNotFound().delete();
+    await VehicleModel.query()
+      .findById(request.params.id)
+      .throwIfNotFound()
+      .delete();
 
-    return reply.redirect(`/vehicles`);
+    return reply.redirect('/vehicles');
   } catch (error) {
     console.error(error);
     return reply.redirect('/vehicles');
