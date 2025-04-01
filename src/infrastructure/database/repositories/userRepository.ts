4c209c0ce2c353099f861e29c5d19f91c96cdf03
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-04-01 16:09:17 -0300

    feat(#1): add user repository
    - Created userRepository with create, update, findById, findByEmail, and findAll methods.

---
+++ src/infrastructure/database/repositories/userRepository.ts
@@ -0,0 +1,64 @@
+import { Result } from '../../../_lib/result.js';
+import { UserModel } from '../models/UserModel.js';
+
+export const UserRepository = {
+  async create(user: UserModel) {
+    try {
+      const createdUser = await UserModel.query().insertAndFetch(user);
+
+      return Result.succeed(createdUser);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Database error';
+
+      return Result.fail<UserModel>({ code: 'DATABASE_ERROR', message });
+    }
+  },
+
+  async update(id: number, user: UserModel) {
+    try {
+      const updatedUser = await UserModel.query().patchAndFetchById(id, user);
+
+      return Result.succeed(updatedUser);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Database error';
+
+      return Result.fail<UserModel>({ code: 'DATABASE_ERROR', message });
+    }
+  },
+
+  async findById(id: number) {
+    try {
+      const user = await UserModel.query().findById(id);
+
+      return Result.succeed(user);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Database error';
+
+      return Result.fail<UserModel>({ code: 'DATABASE_ERROR', message });
+    }
+  },
+
+  async findByEmail(email: string) {
+    try {
+      const user = await UserModel.query().findOne({ email });
+
+      return Result.succeed(user);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Database error';
+
+      return Result.fail<UserModel>({ code: 'DATABASE_ERROR', message });
+    }
+  },
+
+  async findAll() {
+    try {
+      const users = await UserModel.query();
+
+      return Result.succeed(users);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Database error';
+
+      return Result.fail<UserModel[]>({ code: 'DATABASE_ERROR', message });
+    }
+  },
+};
