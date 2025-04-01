440b559212eddcf2b4ff40dc9af8a5327ad45ba1
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-04-01 17:54:41 -0300

    feat(#1): add vehicle factory (WIP)

---
+++ src/factories/vehicleFactory.ts
@@ -0,0 +1,45 @@
+import { faker } from '@faker-js/faker';
+import { Factory } from 'fishery';
+import type { ModelObject } from 'objection';
+import { VehicleModel } from '../infrastructure/database/models/VehicleModel.js';
+import { DealershipFactory } from './dealershipFactory.js';
+
+export const VehicleFactory = Factory.define<
+  ModelObject<VehicleModel>,
+  never,
+  VehicleModel
+>(({ onCreate, sequence, params }) => {
+  onCreate(async (vehicle) => {
+    const dealership = await DealershipFactory.create();
+    vehicle.dealershipId = dealership.id;
+
+    const createdVehicle = await VehicleModel.query()
+      .insertAndFetch(vehicle)
+      .withGraphFetched('dealership');
+
+    return createdVehicle;
+  });
+
+  const {
+    brand = faker.vehicle.manufacturer(),
+    name = faker.vehicle.vehicle(),
+    model = faker.vehicle.model(),
+    year = String(faker.number.int({ min: 1886, max: 2025 })),
+    comments = faker.lorem.words(),
+    dealershipId,
+    dealership,
+  } = params;
+
+  return {
+    id: sequence,
+    brand,
+    name,
+    model,
+    year,
+    comments,
+    dealershipId,
+    dealership,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+});
